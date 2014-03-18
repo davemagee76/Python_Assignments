9a5583fd5c987de036599de6f6bd1d64acbdb6cf
David Magee <david.magee@ucd.ie>
2014-03-18 17:22:16 +0000

    Functions for calculating missing values resolved
    Functions for calculating missing values resolved

--- Assignment_2/assignment_2_new.py
+++ Assignment_2/assignment_2_new.py
@@ -40,42 +40,53 @@ mass_dict = {'H': 1, 'Cl': 35, 'Na': 23, 'C': 12, 'O': 16, 'S': 32, 'Mg': 24, 'C
 # Define a function that generates a new dictionary (called mol_dict) from the lists in the ions variable
 def generate_mol_dict(ions):
     mol_dict = {}
-# each list in the ions variable consists of pairs of elements (ion ID; ion mol weight)
-# each list in ions has a length of even number
-# divide the range of the length of each list by 2, so that the loop will perform the function for each pair
-# i refers to the indices in the lists of the ions variable
-# loop through each list in the ions variable; for each pair of elements assign the first element to a variable called
-# 'key' and the second variable called 'value'
-# return the mol_dict
-# This will generate (dynamically) a new dictionary called mol_dict from the list of elements in the ions variable
-    for i in range(len(ions)//2):
+    # each list in the ions variable consists of pairs of elements (ion ID; ion mol weight)
+    # each list in ions has a length of even number
+    # divide the range of the length of each list by 2, so that the loop will perform the function for each pair
+    # i refers to the indices in the lists of the ions variable
+    # loop through each list in the ions variable; for each pair of elements assign the first element to a variable called
+    # 'key' and the second variable called 'value'
+    # return the mol_dict
+    # This will generate (dynamically) a new dictionary called mol_dict from the list of elements in the ions variable
+    for i in range(len(ions) // 2):
         key = ions[2 * i]
         value = ions[2 * i + 1]
-        mol_dict[key] = int(value) # assign to each key the corresponding molecular weight (convert to an integer)
+        mol_dict[key] = int(value)  # assign to each key the corresponding molecular weight (convert to an integer)
     return mol_dict
 
 
 # Define a function that calculates the molecular weight of each molecule in the mol_dict dictionary
 # This dictionary will call the keys and values assigned in the mass_dict dictionary
 def calc_compound_mol_weight(mol_dict):
-    mol_weight = 0 # before adding to the mol_weight variable, initialise it to zero
-    for ion in list(mol_dict.keys()): # convert the dictionary keys to a list. list(dict.keys()) is a python function
-        mol_weight += int(mol_dict[ion]) * int(mass_dict[ion]) # sum
+    mol_weight = 0  # before adding to the mol_weight variable, initialise it to zero
+    for ion in list(mol_dict.keys()):  # convert the dictionary keys to a list. list(dict.keys()) is a python function
+        mol_weight += int(mol_dict[ion]) * int(mass_dict[ion])  # sum
         #print(int(mol_dict[ion])) # the integer values from the mol_dict dictionary (i.e. no. of ions)
         #print(int(mass_dict[ion])) # the integer values from the mass_dict dictionary (i.e. mol weight of each ion)
     return (mol_weight)
 
 
 # Define a function that solves mass given volume, molarity and the compound molecular weight
-#  mass equals volume*molarity*sum(molecular weight*no. of atoms) for each element in compound
-# mass = volume*molarity*()
+# mass equals volume*molarity*sum(molecular weight*no. of atoms) for each element in compound
+def calc_mass(data):
+    for field in data:
+        grams = (float(volume) / 1000) * float(molarity) * int(compound_mol_weight)
+    return grams
 
 
-#def calc_mass(volume, molarity, ions):
-#    for line in y1:
-#       mass = ((volume/1000)*(molarity)*(calc_mol_weight))
+# Define a function that solves volume given mass, molarity and the compound molecular weight
+# volume equals ((mass/molecular weight)/molarity)
+def calc_vol(data):
+    for field in data:
+        volume = (int(mass) / int(compound_mol_weight)) / float(molarity)
+    return volume
 
-
+# Define a function that solves molarity given mass, volume and the compound molecular weight
+# molarity equals ((mass/molecular weight)/volume)
+def calc_mol(data):
+    for field in data:
+        molarity = (int(mass) / int(compound_mol_weight)) / (int(volume)/1000)
+    return molarity
 
 # Process the input file to perform the calculations:
 # Read in the file from the command line and write to an outfile
@@ -88,85 +99,47 @@ with open(sys.argv[1], 'r') as fin, open(sys.argv[2], 'w') as fout:
         data = data.split('\t')
         #print(data)
 
- #   Sanity check:
-            #   if the length of the each line from the input file is not an even number:
-            #     return an error message and stop the script
-            #     else proceed with the script
+        #   Sanity check:
+        # the length of each list in the data variable should be an odd number
+        # if the length of the each line from the input file is not an even number:
+        # return an error message and stop the script
+        # else proceed with the script
+        if len(data) % 2 == 0:
+            print("expect length of the lists should be an odd number: mass, vol, mol, elements in pairs")
+            sys.exit(2)
+        else:
+            pass
 
-
-
+        # Select only those lines in the input file that contain a "?"
         if "?" in data:
-            #print(data)
             # define the elements in the data variable
             mass = data[0]
             volume = data[1]
             molarity = data[2]
             ions = data[3:]
-            print(ions)
+            #print(mass)
 
+            # Need molecular weights for the calculations:
+            # Generate a dictionary from the ions variable by passing the elements to the generate_mol_dict function
             mol_dict = generate_mol_dict(ions)
-            print(mol_dict)
-
+            #print(mol_dict)
+            # Calculate the molecular weights for each compound in the mol_dict dictionary using
+            # the calc_compound_mol_weight
             compound_mol_weight = calc_compound_mol_weight(mol_dict)
-            print(compound_mol_weight)
+            # print(compound_mol_weight)
 
-
-
-
-
-
-
-
-
-''''
-            #   select only those lines that contain a question mark (i.e. lines needed to solve the problem)
-            if "?" in line:
-                data = data
-                #print(data)
-                if data[0] == "?":
-                    y1 = data
-                    for fields in y1:
-                        mass_1 = y1[0]
-                        volume_1 = y1[1]
-                        molarity_1 = y1[2]
-                        element_1 = y1[3:]
-                    #print(element_1)
-                    #print(y1)
-                    test = calc_mol_weight(element_1)
-                    print(test)
-                elif data [1] == "?":
-                    y2 = data
-                    for fields in y2:
-                        mass_2 = y2[0]
-                        volume_2 = y2[1]
-                        molarity_2 = y2[2]
-                        element_2 = y2[3:]
-                    #test_2 = calc_mol_weight(element_2)
-                    #print (test_2)
-                elif data [2] == "?":
-                    y3 = data
-                    for fields in y3:
-                        mass_3 = y3[0]
-                        volume_3 = y3[1]
-                        molarity_3 = y3[2]'''
-
-
-
-
-
-
-
-# Pass the lines to the correct functions for the calculations:
-#   if '?' is the mass:
-#       then solve the mass problem given volume, molarity and the elements
-#   if '?' is the volume
-#       then solve the volume problem given the mass, molarity and the elements
-#   if '?' is the molarity
-#         then solve the molarity problem given the mass, volume and the elements'''
-
-
-
-
-
-
-
+    # Solve for mass
+            if mass == "?":
+                grams = calc_mass(data)
+                # print(grams)
+                fout.write("mass in grams \n{}\n".format(grams))
+    #Solve for volume
+            if volume == "?":
+                volume = calc_vol(data)
+                # print(volume)
+                #fout.write("volume in litres \n{}\n".format(volume))
+    #Solve for molarity
+            if molarity == "?":
+                molarity = calc_mol(data)
+                #print(molarity)
+                #fout.write("molarity \n{}\n".format(volume))
